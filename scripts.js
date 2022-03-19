6540668a3ffcd9bdaeaf6bcb81c160397bd939d7
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-03-19 10:08:15 +0530

    refactor a little

--- scripts.js
+++ scripts.js
@@ -1,28 +1,30 @@
 const allGameButtons = document.querySelectorAll('.gameArea');
+let clicks = 0;
 
 const gameBoard = (() => {
-    const scoreboard = [];
     const gameboard = ['O', 'X', 'O']
-
     return {
         gameboard,
-        scoreboard
     }
 })();
 
-let clicks = 0;
-
-allGameButtons.forEach((element) => {
-    function printBtnValue() {
-        if (element.innerText == '' && clicks % 2 == 0) {
-            element.innerText = "X";
-            element.disabled = true;
-            clicks ++;
-        } if (element.innerText == '' && clicks % 2 !== 0) {
-            element.innerText = "O";
-            element.disabled = true;
-            clicks ++;
+const playArea = (() => {
+    allGameButtons.forEach((element) => {
+        function printBtnValue() {
+            if (element.innerText == '' && clicks % 2 == 0) {
+                element.innerText = "X";
+                element.disabled = true;
+                clicks ++;
+            } if (element.innerText == '' && clicks % 2 !== 0) {
+                element.innerText = "O";
+                element.disabled = true;
+                clicks ++;
+            }
         }
-    }
-    element.addEventListener('click', printBtnValue);
-});
+        element.addEventListener('click', printBtnValue);
+    })
+})();
+
+const checkVictory = () => {
+
+}
